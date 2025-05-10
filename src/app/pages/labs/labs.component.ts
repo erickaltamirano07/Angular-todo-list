c45abdf420cad883cd93e07e0e5855d991c2ae24
Erick Altamirano <erickaltamirano07@hotmail.com>
2025-05-10 11:24:34 -0500

    Eventos en Angular: click, doble click y cambio de input

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -20,4 +20,12 @@ export class LabsComponent {
     age: 18,
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   };
+
+  clickHandler() {
+    alert('Hola');
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+  }
 }
