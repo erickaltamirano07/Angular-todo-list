002db0a13620da61a3a9284111c667111d23ad77
Erick Altamirano <erickaltamirano07@hotmail.com>
2025-05-13 10:58:37 -0500

    estilos dinámicos en Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -34,6 +34,10 @@ export class LabsComponent {
   });
 
   colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable: true,
+  });
+
   constructor() {
     this.colorCtrl.valueChanges.subscribe((value) => {
       console.log(value);
