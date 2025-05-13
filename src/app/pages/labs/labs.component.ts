42c7e73a08d6bcbafb435263c95e702c798c338e
Erick Altamirano <erickaltamirano07@hotmail.com>
2025-05-13 10:23:06 -0500

    Manejo Avanzado de Formularios Reactivos en Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
@@ -31,6 +32,13 @@ export class LabsComponent {
     age: 5,
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   });
+
+  colorCtrl = new FormControl();
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
 
   clickHandler() {
     alert('Hola');
